46885b84ee4acf68f574e23317367f922f2a705c
Jefferson Stachelski <jeffhsta@riseup.net>
2016-06-22 21:27:52 -0300

    Add a reload TOR config example using stem Controller

--- python_tor.py
+++ python_tor.py
@@ -2,10 +2,12 @@ import getpass
 import requests
 import socket
 import socks
+import stem
 import sys
 import time
 
 from stem.control import Controller
+from stem.connection import MissingPassword, PasswordAuthFailed, AuthenticationFailure
 
 URL = u'https://duckduckgo.com/?q=my+ip&t=ffab&ia=answer'
 PROXY_ADDRESS = u'127.0.0.1'
@@ -19,17 +21,21 @@ def get_tor_controller():
         controller.authenticate()
         return controller
     except stem.SocketError as err:
-        print u'Unable to connect to tor on port 9051: %s' % err
+        print u'Unable to connect to tor on port %d: %s' % (PROXY_CONTROLLER_PORT, err)
         sys.exit(1)
-    except stem.connection.MissingPassword:
+    except MissingPassword:
         pw = getpass.getpass('Controller password: ')
         try:
             controller.authenticate(password=pw)
-        except stem.connection.PasswordAuthFailed:
+            return controller
+        except PasswordAuthFailed:
             print u'Unable to authenticate, password is incorrect'
             sys.exit(1)
-    except stem.connection.AuthenticationFailure as err:
+    except AuthenticationFailure as err:
         print u'Unable to authenticate: %s' % err
+        sys.exit(1)
+    except Exception as err:
+        print u'Unhandle error: %s' % err
         sys.exit(1)
 
 
@@ -44,19 +50,29 @@ def activate_proxy_tor():
                           PROXY_PORT)
     socket.socket = socks.socksocket
 
-
-def main():
-    # activate_proxy_tor()
+def do_example_request_over_tor():
     res = requests.get(URL)
     ip_got = extract_ip_from_response(res.text)
 
     print u'[*] %s' % ip_got
 
-    # TOR controller
 
-    # tor_controller = get_tor_controller()
-    # print u'Tor is running version %s' % tor_controller.get_version()
-    # tor_controller.close()
+def main():
+    tor_controller = get_tor_controller() # this should be before setting up the proxy
+
+    activate_proxy_tor()
+    do_example_request_over_tor()
+
+    # TOR controller
+    print u'[*] Tor is running version %s' % tor_controller.get_version()
+
+    # that reload the TOR config, also the relays that you were using
+    tor_controller.signal(stem.Signal.RELOAD)
+
+    print u'[*] After reload TOR config'
+    do_example_request_over_tor()
+
+    tor_controller.close()
 
 
 if __name__ == '__main__':
